cbc27df459ed3091bc80b91ef2ab124cdc6302bc
bobandevelop <boban@git.org>
2018-04-10 12:03:16 +0200

    change sass folder structure and add main.js

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,8 @@ var sassOptions = {
 var scriptsPaths = [
 	'node_modules/jquery/dist/jquery.min.js', 
 	'node_modules/popper.js/dist/umd/popper.min.js', 
-	'node_modules/bootstrap/dist/js/bootstrap.min.js'	
+	'node_modules/bootstrap/dist/js/bootstrap.min.js',
+	'src/scripts/main.js'	
 ];
 
 gulp.task('sass', function() {
